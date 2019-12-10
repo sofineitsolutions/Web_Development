a226151397ea7872a5db620241a2d906d5f4c01d
shivanishah1997 <52963489+shivanishah1997@users.noreply.github.com>
2019-12-10 13:56:53 +0530

    airspace
    blog-left-sidebar this file is compilited
    variable11,admin11,insert11 those files for blog-left-sidebar

---
+++ airspace/admin11.sql
@@ -0,0 +1,88 @@
+-- phpMyAdmin SQL Dump
+-- version 3.2.0.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Dec 10, 2019 at 08:24 AM
+-- Server version: 5.5.24
+-- PHP Version: 5.3.13
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `air`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin11`
+--
+
+CREATE TABLE IF NOT EXISTS `admin11` (
+  `tit1` text NOT NULL,
+  `des` text NOT NULL,
+  `stit1` text NOT NULL,
+  `sdes1` text NOT NULL,
+  `stit2` text NOT NULL,
+  `sdes2` text NOT NULL,
+  `stit3` text NOT NULL,
+  `sdes3` text NOT NULL,
+  `stit4` text NOT NULL,
+  `sdes4` text NOT NULL,
+  `h1` text NOT NULL,
+  `para1` text NOT NULL,
+  `date1` text NOT NULL,
+  `des1` text NOT NULL,
+  `des2` text NOT NULL,
+  `des3` text NOT NULL,
+  `des4` text NOT NULL,
+  `h2` text NOT NULL,
+  `para2` text NOT NULL,
+  `date2` text NOT NULL,
+  `des5` text NOT NULL,
+  `des6` text NOT NULL,
+  `des7` text NOT NULL,
+  `des8` text NOT NULL,
+  `h3` text NOT NULL,
+  `para3` text NOT NULL,
+  `date3` text NOT NULL,
+  `des9` text NOT NULL,
+  `des10` text NOT NULL,
+  `des11` text NOT NULL,
+  `des12` text NOT NULL,
+  `h4` text NOT NULL,
+  `para4` text NOT NULL,
+  `date4` text NOT NULL,
+  `des13` text NOT NULL,
+  `des14` text NOT NULL,
+  `des15` text NOT NULL,
+  `des16` text NOT NULL,
+  `h5` text NOT NULL,
+  `para5` text NOT NULL,
+  `date5` text NOT NULL,
+  `des17` text NOT NULL,
+  `des18` text NOT NULL,
+  `des19` text NOT NULL,
+  `des20` text NOT NULL,
+  `h6` text NOT NULL,
+  `para6` text NOT NULL,
+  `date6` text NOT NULL,
+  `des21` text NOT NULL,
+  `des22` text NOT NULL,
+  `des23` text NOT NULL,
+  `des24` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin11`
+--
+
+INSERT INTO `admin11` (`tit1`, `des`, `stit1`, `sdes1`, `stit2`, `sdes2`, `stit3`, `sdes3`, `stit4`, `sdes4`, `h1`, `para1`, `date1`, `des1`, `des2`, `des3`, `des4`, `h2`, `para2`, `date2`, `des5`, `des6`, `des7`, `des8`, `h3`, `para3`, `date3`, `des9`, `des10`, `des11`, `des12`, `h4`, `para4`, `date4`, `des13`, `des14`, `des15`, `des16`, `h5`, `para5`, `date5`, `des17`, `des18`, `des19`, `des20`, `h6`, `para6`, `date6`, `des21`, `des22`, `des23`, `des24`) VALUES
+('Blog', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi, quibusdam.', 'Introducing Swift for Mac', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis, officia.', 'Welcome to Themefisher Family', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis, officia.', 'Warm welcome from swift', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis, officia.', 'Introducing Swift for Mac', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis, officia.', 'How To Wear Bright Shoes', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit vitae placeat ad architecto nostrum asperiores vel aperiam, veniam eum nulla. Maxime cum magnam, adipisci architecto quibusdam cumque veniam fugiat quae. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio vitae ab doloremque accusamus sit, eos dolorum officiis a perspiciatis aliquid. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, facere.', '20, MAR 2017', 'POSTED BY ADMIN', 'LIFESTYLE', 'TRAVEL', 'FASHION', 'Two Ways To Wear Straight Shoes', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit vitae placeat ad architecto nostrum asperiores vel aperiam, veniam eum nulla. Maxime cum magnam, adipisci architecto quibusdam cumque veniam fugiat quae. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio vitae ab doloremque accusamus sit, eos dolorum officiis a perspiciatis aliquid. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, facere.', '20, MAR 2017', 'POSTED BY ADMIN', 'LIFESTYLE', 'TRAVEL', 'FASHION', 'Making A Denim Statement', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit vitae placeat ad architecto nostrum asperiores vel aperiam, veniam eum nulla. Maxime cum magnam, adipisci architecto quibusdam cumque veniam fugiat quae. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio vitae ab doloremque accusamus sit, eos dolorum officiis a perspiciatis aliquid. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, facere.', '20, MAR 2017', 'POSTED BY ADMIN', 'LIFESTYLE', 'TRAVEL', 'FASHION', 'Standard Text Post', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit vitae placeat ad architecto nostrum asperiores vel aperiam, veniam eum nulla. Maxime cum magnam, adipisci architecto quibusdam cumque veniam fugiat quae. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio vitae ab doloremque accusamus sit, eos dolorum officiis a perspiciatis aliquid. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, facere.', '20, MAR 2017', 'POSTED BY ADMIN', 'LIFESTYLE', 'TRAVEL', 'FASHION', 'Standard Audio Post', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit vitae placeat ad architecto nostrum asperiores vel aperiam, veniam eum nulla. Maxime cum magnam, adipisci architecto quibusdam cumque veniam fugiat quae. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio vitae ab doloremque accusamus sit, eos dolorum officiis a perspiciatis aliquid. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, facere.', '20, MAR 2017', 'POSTED BY ADMIN', 'LIFESTYLE', 'TRAVEL', 'FASHION', 'Standard vidio Post', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit vitae placeat ad architecto nostrum asperiores vel aperiam, veniam eum nulla. Maxime cum magnam, adipisci architecto quibusdam cumque veniam fugiat quae. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio vitae ab doloremque accusamus sit, eos dolorum officiis a perspiciatis aliquid. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, facere.', '20, MAR 2017', 'POSTED BY ADMIN', 'LIFESTYLE', 'TRAVEL', 'FASHION');
