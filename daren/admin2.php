fe3bad9a2169b7c76652fadf49011bf238a5bba8
shivanishah1997 <52963489+shivanishah1997@users.noreply.github.com>
2019-12-14 18:02:38 +0530

    daren category file complited
    category file is done variable 2 file,insert 2,admin2 this file is created

---
+++ daren/admin2.php
@@ -0,0 +1,235 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>DarEn BLOG || CONTACT</title>
+  <link rel="icon" href="img/favicon.png">
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <!-- animate CSS -->
+  <link rel="stylesheet" href="css/animate.css">
+  <!-- owl carousel CSS -->
+  <link rel="stylesheet" href="css/owl.carousel.min.css">
+  <!-- themify CSS -->
+  <link rel="stylesheet" href="css/themify-icons.css">
+  <!-- flaticon CSS -->
+  <link rel="stylesheet" href="css/liner_icon.css">
+  <link rel="stylesheet" href="css/search.css">
+  <!-- swiper CSS -->
+  <link rel="stylesheet" href="css/slick.css">
+  <!-- style CSS -->
+  <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+<?php include("variable2.php");
+            include("h.php");?>
+
+  <!-- breadcrumb start-->
+  <section class="breadcrumb breadcrumb_bg">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="breadcrumb_iner text-center">
+            <div class="breadcrumb_iner_item">
+              <h2>Contact Us</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- breadcrumb start-->
+
+        <!--================Contact Area =================-->
+        <section class="contact_area p_120">
+            <div class="container">
+        
+                <div class="row">    
+                    <div class="col-lg-9">
+                        <form class="row contact_form" action="insert2.php" method="post">
+                            <div class="col-md-6">
+                            
+                    <div class="form-group" >
+                        <input type="text" class="form-control" id="name" name="title1" placeholder="Enter description">
+                    </div>
+                    <div class="form-group" >
+                        <input type="text" class="form-control" id="name" name="name1" placeholder="Enter description">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" id="name" name="date1" placeholder="click on">
+                    </div>
+                    <div class="form-group">
+                        <textarea  type="text" class="form-control" id="name" name="stitle1" placeholder="click on"></textarea >
+                    </div>
+                    <div class="form-group" >
+                        <input type="text" class="form-control" id="name" name="title2" placeholder="Enter description">
+                    </div>
+                    <div class="form-group" >
+                        <input type="text" class="form-control" id="name" name="name2" placeholder="Enter description">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" id="name" name="date2" placeholder="click on">
+                    </div>
+                    <div class="form-group">
+                        <textarea type="text" class="form-control" id="name" name="stitle2" placeholder="click on"></textarea >
+                    </div>
+                    <div class="form-group" >
+                        <input type="text" class="form-control" id="name" name="title3" placeholder="Enter description">
+                    </div>
+                    <div class="form-group" >
+                        <input type="text" class="form-control" id="name" name="name3" placeholder="Enter description">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" id="name" name="date3" placeholder="click on">
+                    </div>
+                    <div class="form-group">
+                        <textarea type="text" class="form-control" id="name" name="stitle3" placeholder="click on"></textarea >
+                    </div>
+                    <div class="form-group" >
+                        <input type="text" class="form-control" id="name" name="title4" placeholder="Enter description">
+                    </div>
+                    <div class="form-group" >
+                        <input type="text" class="form-control" id="name" name="name4" placeholder="Enter description">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" id="name" name="date4" placeholder="click on">
+                    </div>
+                    <div class="form-group">
+                        <textarea type="text" class="form-control" id="name" name="stitle4" placeholder="click on"></textarea >
+                    </div>
+                    <div class="form-group" >
+                        <input type="text" class="form-control" id="name" name="title5" placeholder="Enter description">
+                    </div>
+                    <div class="form-group" >
+                        <input type="text" class="form-control" id="name" name="name5" placeholder="Enter description">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" id="name" name="date5" placeholder="click on">
+                    </div>
+                    <div class="form-group">
+                        <textarea type="text" class="form-control" id="name" name="stitle5" placeholder="click on"></textarea >
+                    </div>
+                    <div class="form-group" >
+                        <input type="text" class="form-control" id="name" name="name6" placeholder="Enter description">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" id="name" name="date6" placeholder="click on">
+                    </div>
+                    <div class="form-group">
+                        <textarea type="text" class="form-control" id="name" name="stitle6" placeholder="click on"></textarea >
+                    </div>
+                    <div class="form-group" >
+                        <input type="text" class="form-control" id="name" name="name7" placeholder="Enter description">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" id="name" name="date7" placeholder="click on">
+                    </div>
+                    <div class="form-group">
+                        <textarea type="text" class="form-control" id="name" name="stitle7" placeholder="click on"></textarea >
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" id="name" name="name8" placeholder="click on">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" id="name" name="date8" placeholder="click on">
+                    </div>       
+                    <div class="form-group" >
+                        <textarea type="text" class="form-control" id="name" name="stitle8" placeholder="Enter description"></textarea >
+                    </div>    
+                      <div class="load_btn">
+                            <button type="submit" value="submit"   class="btn_1">Send Message</button>
+                    </div>
+                    </div>
+                           <!-- <a class="nav-link" href="admin.php">update</a>--> 
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+  <!-- ================ contact section end ================= -->
+
+  <!-- footer part start-->
+  <footer class="footer-area">
+    <div class="container">
+      <div class="row">
+        <div class="col-xl-4 col-sm-6">
+          <div class="single-footer-widget footer_1">
+            <h4>About Me</h4>
+            <p>Do you want to be even more successful? Learn to love learning and growth.
+              The more effort you put into improving your skills, the bigger the payoff
+              you will get. Realize that things will be hard at first, but the rewards
+              will be worth it.</p>
+          </div>
+        </div>
+        <div class="col-xl-4 col-sm-6">
+          <div class="single-footer-widget footer_2">
+            <h4>Contact us</h4>
+            <div class="contact_info">
+              <span class="ti-home"></span>
+              <h5>Los angeles, United States</h5>
+              <p>659, Rocky beach bullevard, santa monica, Rocky beach, USA.</p>
+            </div>
+            <div class="contact_info">
+              <span class="ti-headphone-alt"></span>
+              <h5>+44 6532 986 652</h5>
+              <p>Mon to Fri 9am to 6 pm.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-xl-4 col-sm-8">
+          <div class="single-footer-widget footer_3">
+            <h4>Newsletter</h4>
+            <p>Stay updated with our latest trends The more effort you put into
+              improving your skills, the bigger the payoff you will get realize that things.</p>
+            <form action="#">
+              <div class="form-group">
+                <div class="input-group mb-3">
+                  <input type="text" class="form-control" placeholder='Enter email address'
+                    onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter email address'">
+                  <div class="input-group-append">
+                    <button class="btn" type="button"><span class="lnr lnr-arrow-right"></span></button>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+      <div class="row align-items-center">
+        <div class="col-lg-12">
+          <div class="copyright_part_text text-center">
+            <p class="footer-text m-0"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="ti-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <!-- footer part end-->
+
+  <!-- jquery plugins here-->
+  <!-- jquery -->
+  <script src="js/jquery-1.12.1.min.js"></script>
+  <!-- popper js -->
+  <script src="js/popper.min.js"></script>
+  <!-- bootstrap js -->
+  <script src="js/bootstrap.min.js"></script>
+  <!-- particles js -->
+  <script src="js/contact.js"></script>
+  <!-- ajaxchimp js -->
+  <script src="js/jquery.ajaxchimp.min.js"></script>
+  <!-- validate js -->
+  <script src="js/jquery.validate.min.js"></script>
+  <!-- form js -->
+  <script src="js/jquery.form.js"></script>
+  <script src="js/mail-script.js"></script>
+  <!-- custom js -->
+  <script src="js/custom.js"></script>
+</body>
+
+</html>
---
+++ daren/category.sql
@@ -0,0 +1,65 @@
+-- phpMyAdmin SQL Dump
+-- version 3.2.0.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Dec 14, 2019 at 12:28 PM
+-- Server version: 5.5.24
+-- PHP Version: 5.3.13
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `daren`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE IF NOT EXISTS `category` (
+  `title1` text NOT NULL,
+  `name1` text NOT NULL,
+  `date1` text NOT NULL,
+  `stitle1` text NOT NULL,
+  `title2` text NOT NULL,
+  `name2` text NOT NULL,
+  `date2` text NOT NULL,
+  `stitle2` text NOT NULL,
+  `title3` text NOT NULL,
+  `name3` text NOT NULL,
+  `date3` text NOT NULL,
+  `stitle3` text NOT NULL,
+  `title4` text NOT NULL,
+  `name4` text NOT NULL,
+  `date4` text NOT NULL,
+  `stitle4` text NOT NULL,
+  `title5` text NOT NULL,
+  `name5` text NOT NULL,
+  `date5` text NOT NULL,
+  `stitle5` text NOT NULL,
+  `name6` text NOT NULL,
+  `date6` text NOT NULL,
+  `stitle6` text NOT NULL,
+  `name7` text NOT NULL,
+  `date7` text NOT NULL,
+  `stitle7` text NOT NULL,
+  `name8` text NOT NULL,
+  `date8` text NOT NULL,
+  `stitle8` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`title1`, `name1`, `date1`, `stitle1`, `title2`, `name2`, `date2`, `stitle2`, `title3`, `name3`, `date3`, `stitle3`, `title4`, `name4`, `date4`, `stitle4`, `title5`, `name5`, `date5`, `stitle5`, `name6`, `date6`, `stitle6`, `name7`, `date7`, `stitle7`, `name8`, `date8`, `stitle8`) VALUES
+('abstract design', 'By Michal', '/ March 30 , 2019', 'Called face there light this said wherein replenish sadin made green grass', 'abstract design', 'By Michal', '/ March 30 , 2019', 'Called face there light this said wherein replenish sadin made green grass', 'abstract design', 'By Michal', '/ March 30 , 2019', 'Called face there light this said wherein replenish sadin made green grass', 'abstract design', 'By Michal', '/ March 30 , 2019', 'Called face there light this said wherein replenish sadin made green grass', 'abstract design', 'By Michal', '/ March 30 , 2019', 'Called face there light this said wherein replenish sadin made green grass', 'By Michal', '/ March 30 , 2019', 'Subdue lesser beast winged bearing meat tree one', 'By Michal', '/ March 30 , 2019', 'Subdue lesser beast winged bearing meat tree one', 'By Michal', '/ March 30 , 2019', 'Subdue lesser beast winged bearing meat tree one');
