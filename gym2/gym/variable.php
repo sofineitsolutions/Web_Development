25da3013d93b580405399e4ef4ad1252fcf7062d
shivanishah1997 <52963489+shivanishah1997@users.noreply.github.com>
2019-12-26 22:58:00 +0530

    gym2

---
+++ gym2/gym/variable.php
@@ -0,0 +1,66 @@
+<?php
+$title1="Build Up Your";
+$title2="Body Shape";
+$title3="Build Your Body and Fitness with Professional Touch";
+$title4="Build Up Your";
+$title5="Strength";
+$title6="Build Your Body and Fitness with Professional Touch";
+$title7="Build Up Your";
+$title8="Body Shape";
+$title9="Build Your Body and Fitness with Professional Touch";
+$title10="Build Up Your";
+$title11="Strength";
+$title12="Build Your Body and Fitness with Professional Touch";
+
+$stitle1="For Girl";
+$sdes1="There are many variations of passages of lorem <br> Ipsum available, but the majority have suffered <br> alteration.";
+$stitle2="For Boys";
+$sdes2="There are many variations of passages of lorem <br> Ipsum available, but the majority have suffered <br> alteration.";
+$title13="Our Features";
+$des1="There are many variations of passages of lorem Ipsum available, but the majority <br> have suffered alteration.";
+$ftitle1="Weightlifting";
+$fdes1="There are many variations of passages of lorem Ipsum available.";
+$ftitle2="Specific Muscles";
+$fdes2="There are many variations of passages of lorem Ipsum available.";
+$ftitle3="Flex Your Muscles";
+$fdes3="There are many variations of passages of lorem Ipsum available.";
+$ftitle4="Cardio Exercises";
+$fdes4="There are many variations of passages of lorem Ipsum available.";
+$title14="A Big Offer for <br>This Summer";
+$dis="50% Off";
+$des2="There are many variations of passages of lorem Ipsum available, but the majority have suffered alteration.";
+$title15="Our Pricing";
+$des3="There are many variations of passages of lorem Ipsum available, but the majority <br> have suffered alteration.";
+$ptitle1="Beginner";
+$price1="$45/m";
+$pdes1="24h unlimited access";
+$pdes2="Trainer Advice";
+$pdes3="Locker + Bathroom";
+$pdes4="Personal trainer";
+$ptitle2="Expert";
+$price2="$45/m";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
